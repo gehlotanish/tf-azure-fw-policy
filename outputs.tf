89b4d2a7caae30282c7feb92599db8db8faaace5
Anish gehlot <anishgehlot@gmail.com>
2025-07-07 12:16:39 +0530

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,9 @@
-# Placeholder for Terraform code
-# Should contain all output variables of modules
+output "firewall_policy_id" {
+  description = "ID of the Azure Firewall Policy."
+  value       = azurerm_firewall_policy.main.id
+}
+
+output "firewall_policy_base_policy_id" {
+  description = "ID of the Azure Firewall Policy."
+  value       = azurerm_firewall_policy.main.base_policy_id
+}
